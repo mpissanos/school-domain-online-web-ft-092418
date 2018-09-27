78c5802d7ef46a00b570579ec573c6e069e4ed0c
Manny Pissanos <mpissano@gmail.com>
2018-09-27 20:31:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  
+  attr_accessor :grade, :student_name
+  attr_reader :school_name, :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    if @roster[grade] == grade
+    @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade]<< student_name
+    end
+  end
+   
+  
+  
+  
+    
+  
+  
+end
