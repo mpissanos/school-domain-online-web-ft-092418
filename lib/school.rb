2ce4c635e20c66e3f8115696bb3ece42d2e2894c
Manny Pissanos <mpissano@gmail.com>
2018-09-30 01:07:55 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,7 @@ class School
   def add_student(student_name, grade)
    @roster.each do |x| 
      if x == grade
-       @roster[x] << student_name
+       @roster[grade] << student_name
      else
       @roster[grade] = []
       @roster[grade]<< student_name
