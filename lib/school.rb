ea0811a9217638526c511a19f1b4313a912d7c53
Manny Pissanos <mpissano@gmail.com>
2018-09-30 01:49:15 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 class School
   
-  attr_accessor :grade, :student_name, :roster
-  attr_reader :school_name, 
+  attr_accessor :grade, :student_name, 
+  attr_reader :school_name, :roster
   
   def initialize(school_name)
     @school_name = school_name
