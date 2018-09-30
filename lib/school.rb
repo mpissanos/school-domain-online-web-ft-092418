5a966bfbbe36e9bfd188c5bb32cf08456de03bdc
Manny Pissanos <mpissano@gmail.com>
2018-09-30 01:51:10 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -21,3 +21,10 @@ class School
 #   end
 # end
   
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
